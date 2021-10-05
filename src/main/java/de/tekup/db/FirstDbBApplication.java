81180a660182d6a3bda69180624c5b20bce93bec
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-05 10:29:45 +0100

    project init

---
+++ src/main/java/de/tekup/db/FirstDbBApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDbBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDbBApplication.class, args);
+	}
+
+}
