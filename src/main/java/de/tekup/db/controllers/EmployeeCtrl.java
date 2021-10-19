2f39bba034ceeaad41297e5314425c81fba6f37d
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-19 09:41:35 +0100

    Update EmployeeCtrl.java

--- src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
+++ src/main/java/de/tekup/db/controllers/EmployeeCtrl.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +38,10 @@ public class EmployeeCtrl {
 		return empService.getEmployeeById(id);
 	}
 	
+	@PutMapping("employee/update/{id}")
+	public EmployeeEntity updateOnefromDB(@PathVariable int id,@RequestBody EmployeeEntity employeeEntity){
+		return empService.updateEmployeeById(id, employeeEntity);
+	}
 	
 	@ExceptionHandler(NoSuchElementException.class)
 	public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e){
